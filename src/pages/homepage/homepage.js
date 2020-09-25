c2f85f8c6b08fe850d4d6e7fb0fc2c7e7fa8dd47
Arpit Tomar <arpittomar7740@gmail.com>
2020-09-25 19:00:15 +0530

    first home page

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./homepage.scss";
+import Directory from "../../components/directory/directory";
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory></Directory>
+  </div>
+);
+
+export default Homepage;
