756bf359df72eff68786ce7805235c9a6ce4b65c
Arpit Tomar <arpittomar7740@gmail.com>
2020-09-27 02:59:31 +0530

    Adding items to shopcart

--- src/components/cart/cart-dropdown/cart-dropdown.js
+++ src/components/cart/cart-dropdown/cart-dropdown.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "./cart-dropdown.scss";
 import CustomButton from "../../CustomButton/CustomButton";
+import { connect } from "react-redux";
+import CartItem from "../../cart-item/cart-item";
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
+  console.log(cartItems);
   return (
     <div className="cart-dropdown">
-      <div className="cart-item"></div>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} {...item}></CartItem>
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
